8a5f8478f6cf95e80391dfdffd51f2a1be6810ea
Crowdbotics <team@crowdbotics.com>
2023-05-05 21:58:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-frog-40486",
+  url: "https://rapid-frog-40486.botics.co",
+  api: "https://rapid-frog-40486.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
